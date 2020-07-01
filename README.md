83abe66c13fa82c64ec59611f0868296c6f4e59f
Pedro Henrique <system.pedrohenrique@gmail.com>
2020-07-01 10:53:42 -0300

    wip

--- README.md
+++ README.md
@@ -1,46 +1,10 @@
 ## VK Group Backup
 
-Docker images for making backup of all topics from a VK Group/Community.
+*NOTE: For the RabbitMQ version check the tag [v1.0.0](https://github.com/crossworth/vk-group-backup/tree/v1.0.0)*
 
-To run it you will need an `RabbitMQ` instance, a `MongoDB` and Docker.
+Downloads topics from a VK Group/Board/Community to JSON format.
 
-### Steps
-1. Login on Github docker registry
-2. Start the `queue all topics` image, it will  get all the topics id from the given community and send to rabbitmq.
-3. Start the `worker` image, it will save a topic at time from rabbitmq to mongodb.
+WIP
 
-
-Example running the `queue all topics`
-```shell script
-docker run -d \
--e VK_GROUP_ID=0 \ 
--e VK_EMAIL=email@goes.here \
--e VK_PASSWORD=password  \
--e RABBITMQ_SERVER=amqp://localhost:5672 \ 
--e RABBITMQ_QUEUE_NAME=queue-name \
-docker.pkg.github.com/crossworth/vk-group-backup/queue-all-topics:latest
-```
-
-Example running the `worker`
-```shell script
-docker run -d \
--e MONGO_SERVER=mongodb://localhost:27017 \ 
--e MONGO_DATABASE=mongodb \
--e VK_GROUP_ID=0 \ 
--e VK_EMAIL=email@goes.here \
--e VK_PASSWORD=password  \
--e RABBITMQ_SERVER=amqp://localhost:5672 \ 
--e RABBITMQ_QUEUE_NAME=queue-name \
-docker.pkg.github.com/crossworth/vk-group-backup/worker:latest
-```
-
-#### NOTES
-You can change the code to use `go channels` instead of `RabbitMQ` and `JSON` instead of `MongoDB`, making it easy to deploy.
-
-You can change/create a new process to handle the recent topics to be able to make live updates of topics, the function `GetRecentTopicsIDs` is already implemented.
-
-You can have more then 1 worker, but you must define an environment variable called `VK_DEVICE` with `iphone`, `androind` or `wphone`.
-If you try make a lot of request from the same device you will be rate limited/blocked for 2 hours.
-
-If you are a group administrator you can use `LongPolling` from `VKAPI`, its better and easy than using this project. 
+*NOTE: If you are a group administrator you can use `LongPolling` from `VKAPI`, its better and easy than using this project.* 
 
--- topic.go
+++ topic.go
@@ -1,78 +1,25 @@
 package vgb
 
 import (
-	"fmt"
+	"context"
 	"net/url"
 	"strconv"
 	"time"
 
 	vkapi "github.com/himidori/golang-vk-api"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Profile struct {
-	ID         int    `json:"id" bson:"id"`
-	FirstName  string `json:"first_name" bson:"first_name"`
-	LastName   string `json:"last_name" bson:"last_name"`
-	ScreenName string `json:"screen_name" bson:"screen_name"`
-	Photo      string `json:"photo" bson:"photo"`
-}
-
-type Comment struct {
-	ID          int      `json:"id" bson:"id"`
-	FromID      int      `json:"from_id" bson:"from_id"`
-	Date        int64    `json:"date" bson:"date"`
-	Text        string   `json:"text" bson:"text"`
-	Likes       int      `json:"likes" bson:"likes"`
-	ReplyToUID  int      `json:"reply_to_uid" bson:"reply_to_uid"`
-	ReplyToCID  int      `json:"reply_to_cid" bson:"reply_to_cid"`
-	Attachments []string `json:"attachments" bson:"attachments"`
-}
-
-type Poll struct {
-	ID       int          `json:"id" bson:"id"`
-	Question string       `json:"question" bson:"question"`
-	Votes    int          `json:"votes" bson:"votes"`
-	Answers  []PollAnswer `json:"answers" bson:"answers"`
-	Multiple bool         `json:"multiple" bson:"multiple"`
-	EndDate  int64        `json:"end_date" bson:"end_date"`
-	Closed   bool         `json:"closed" bson:"closed"`
-}
-
-type PollAnswer struct {
-	ID    int     `json:"id" bson:"id"`
-	Text  string  `json:"text" bson:"text"`
-	Votes int     `json:"votes" bson:"votes"`
-	Rate  float64 `json:"rate" bson:"rate"`
-}
-
-type Topic struct {
-	ID        int             `json:"id" bson:"id"`
-	Title     string          `json:"title" bson:"title"`
-	IsClosed  bool            `json:"is_closed" bson:"is_closed"`
-	IsFixed   bool            `json:"is_fixed" bson:"is_fixed"`
-	CreatedAt int64           `json:"created_at" bson:"created_at"`
-	UpdatedAt int64           `json:"updated_at" bson:"updated_at"`
-	CreatedBy Profile         `json:"created_by" bson:"created_by"`
-	UpdatedBy Profile         `json:"updated_by" bson:"updated_by"`
-	Profiles  map[int]Profile `json:"profiles" bson:"profiles"`
-	Poll      Poll            `json:"poll" bson:"poll"`
-	Comments  []Comment       `json:"comments" bson:"comments"`
-}
-
-func (v *VKGroupBackUp) GetRecentTopicsIDs() (<-chan int, <-chan error) {
+func GetRecentTopicsIDs(ctx context.Context, client *vkapi.VKClient, pullRecentInterval time.Duration, groupID int) (<-chan int, <-chan error) {
 	topicIDChan := make(chan int)
 	errorChan := make(chan error)
 
 	go func() {
 		for {
 			select {
-			case <-time.After(v.pullRecentInterval):
-
+			case <-time.After(pullRecentInterval):
 				params := url.Values{}
 				params.Set("order", "1")
-				topics, err := v.client.BoardGetTopics(v.groupID, 100, params)
+				topics, err := client.BoardGetTopics(groupID, 100, params)
 				if err != nil {
 					errorChan <- err
 					return
@@ -82,7 +29,7 @@ func (v *VKGroupBackUp) GetRecentTopicsIDs() (<-chan int, <-chan error) {
 					topicIDChan <- topics.Topics[i].ID
 				}
 
-			case <-v.ctx.Done():
+			case <-ctx.Done():
 				close(topicIDChan)
 				close(errorChan)
 				return
@@ -93,7 +40,7 @@ func (v *VKGroupBackUp) GetRecentTopicsIDs() (<-chan int, <-chan error) {
 	return topicIDChan, errorChan
 }
 
-func (v *VKGroupBackUp) GetAllTopicIds() (<-chan int, <-chan error) {
+func GetAllTopicIds(ctx context.Context, client *vkapi.VKClient, pullRecentInterval time.Duration, groupID int) (<-chan int, <-chan error) {
 	topicIDChan := make(chan int)
 	errorChan := make(chan error)
 
@@ -105,7 +52,7 @@ func (v *VKGroupBackUp) GetAllTopicIds() (<-chan int, <-chan error) {
 
 		for {
 			params.Set("offset", strconv.Itoa(skip))
-			topics, err := v.client.BoardGetTopics(v.groupID, 100, params)
+			topics, err := client.BoardGetTopics(groupID, 100, params)
 			if err != nil {
 				errorChan <- err
 				continue
@@ -128,211 +75,4 @@ func (v *VKGroupBackUp) GetAllTopicIds() (<-chan int, <-chan error) {
 	}()
 
 	return topicIDChan, errorChan
-}
-
-func (v *VKGroupBackUp) SaveTopic(topicID int) (bool, error) {
-	topicsCollection := v.mongo.Database(v.mongoDatabase).Collection("topics")
-
-	params := url.Values{}
-	params.Set("topic_ids", strconv.Itoa(topicID))
-	params.Set("extended", "1")
-	topicResult, err := v.client.BoardGetTopics(v.groupID, 1, params)
-	if err != nil {
-		return false, err
-	}
-
-	if len(topicResult.Topics) < 0 {
-		panic("len(topicResult.Topics) < 0")
-	}
-
-	topicFilter := bson.D{{
-		"id", topicID,
-	}}
-
-	var topic Topic
-	var updating bool
-
-	err = topicsCollection.FindOne(v.ctx, topicFilter).Decode(&topic)
-	if err != nil && err != mongo.ErrNoDocuments {
-		return false, err
-	}
-
-	// NOTE(Pedro): Topic updated
-	if topic.ID != 0 && topic.UpdatedAt == topicResult.Topics[0].Updated {
-		fmt.Printf("Topic ID = %d already updated\n", topic.ID)
-		return true, nil
-	}
-
-	if topic.ID != 0 {
-		updating = true
-	}
-
-	profilesUsers := mapUsers(topicResult.Profiles)
-
-	topic.ID = topicID
-	topic.Title = topicResult.Topics[0].Title
-	topic.IsClosed = intToBool(topicResult.Topics[0].IsClosed)
-	topic.IsFixed = intToBool(topicResult.Topics[0].IsFixed)
-	topic.CreatedAt = topicResult.Topics[0].Created
-	topic.CreatedBy = vkUserToProfile(profilesUsers[topicResult.Topics[0].CreatedBy])
-	topic.UpdatedAt = topicResult.Topics[0].Updated
-	topic.UpdatedBy = vkUserToProfile(profilesUsers[topicResult.Topics[0].UpdatedBy])
-
-	if topic.Profiles == nil {
-		topic.Profiles = make(map[int]Profile)
-	}
-
-	commentsParams := url.Values{}
-	commentsParams.Set("extended", "1")
-	commentsParams.Set("need_likes", "1")
-
-	if len(topic.Comments) > 0 {
-		params.Set("start_comment_id", strconv.Itoa(len(topic.Comments)))
-	}
-
-	for {
-		comments, err := v.client.BoardGetComments(v.groupID, topicID, 100, commentsParams)
-		if err != nil {
-			return false, err
-		}
-
-		if comments.Poll != nil {
-			topic.Poll = Poll{
-				ID:       comments.Poll.ID,
-				Question: comments.Poll.Question,
-				Votes:    comments.Poll.Votes,
-				Answers:  vkPollAnswerToPollAnswer(comments.Poll.Answers),
-				Multiple: comments.Poll.Multiple,
-				EndDate:  comments.Poll.EndDate,
-				Closed:   comments.Poll.Closed,
-			}
-		}
-
-		// NOTE(Pedro): This save the profiles without duplicating it
-		for i := range comments.Profiles {
-			topic.Profiles[comments.Profiles[i].UID] = vkUserToProfile(*comments.Profiles[i])
-		}
-
-		for i := range comments.Comments {
-			topic.Comments = append(topic.Comments, vkCommentToComment(*comments.Comments[i]))
-		}
-
-		fmt.Printf("%d len(topic.Comments) >=  %d comments.Count\n", len(topic.Comments), comments.Count)
-		if len(topic.Comments) >= comments.Count {
-			break
-		}
-	}
-
-	if updating {
-		_, err = topicsCollection.UpdateOne(v.ctx, topicFilter, topic)
-	} else {
-		_, err = topicsCollection.InsertOne(v.ctx, topic)
-	}
-
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
-}
-
-func intToBool(i int) bool {
-	if i > 0 {
-		return true
-	}
-
-	return false
-}
-
-func vkUserToProfile(user vkapi.User) Profile {
-	return Profile{
-		ID:         user.UID,
-		FirstName:  user.FirstName,
-		LastName:   user.LastName,
-		ScreenName: user.ScreenName,
-		Photo:      user.Photo100,
-	}
-}
-
-func mapUsers(profiles []*vkapi.User) map[int]vkapi.User {
-	users := make(map[int]vkapi.User)
-
-	for i := range profiles {
-		users[profiles[i].UID] = *profiles[i]
-	}
-
-	return users
-}
-
-func vkCommentToComment(comment vkapi.TopicComment) Comment {
-	cmt := Comment{
-		ID:         comment.ID,
-		FromID:     comment.FromID,
-		Date:       comment.Date,
-		Text:       comment.Text,
-		ReplyToUID: comment.ReplyToUID,
-		ReplyToCID: comment.ReplyToUID,
-	}
-
-	if comment.Likes != nil {
-		cmt.Likes = comment.Likes.Count
-	}
-
-	for i := range comment.Attachments {
-		switch comment.Attachments[i].Type {
-		case "photo":
-			cmt.Attachments = append(cmt.Attachments, getBestPhoto(*comment.Attachments[i].Photo))
-		case "sticker":
-			cmt.Attachments = append(cmt.Attachments, getBestSticker(*comment.Attachments[i].Sticker))
-		case "video":
-			cmt.Attachments = append(cmt.Attachments, fmt.Sprintf("https://vk.com/video?z=video%d_%d%%2F%s", comment.Attachments[i].Video.OwnerID, comment.Attachments[i].Video.ID, comment.Attachments[i].Video.AccessKey))
-		case "audio":
-			cmt.Attachments = append(cmt.Attachments, comment.Attachments[i].Audio.Url)
-		}
-	}
-
-	return cmt
-}
-
-func getBestPhoto(attachment vkapi.AttachmentPhoto) string {
-	best := attachment.Sizes[0]
-
-	for i := range attachment.Sizes {
-		s := attachment.Sizes[i].Width * attachment.Sizes[i].Height
-		b := best.Width * best.Height
-		if s > b {
-			best = attachment.Sizes[i]
-		}
-	}
-
-	return best.Url
-}
-
-func getBestSticker(attachment vkapi.AttachmentSticker) string {
-	best := attachment.Images[0]
-
-	for i := range attachment.Images {
-		s := attachment.Images[i].Width * attachment.Images[i].Height
-		b := best.Width * best.Height
-		if s > b {
-			best = attachment.Images[i]
-		}
-	}
-
-	return best.Url
-}
-
-func vkPollAnswerToPollAnswer(answers []*vkapi.PollAnswer) []PollAnswer {
-	var result []PollAnswer
-
-	for i := range answers {
-		result = append(result, PollAnswer{
-			ID:    answers[i].ID,
-			Text:  answers[i].Text,
-			Votes: answers[i].Votes,
-			Rate:  answers[i].Rate,
-		})
-	}
-
-	return result
 }
